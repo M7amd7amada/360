d9fa9b028cf63339755e0c623934f298a1c945b7
Nabil Mohamed Elsadany <143938749+nabil-elsadany@users.noreply.github.com>
2025-08-21 10:54:46 +0300

    remove un needed using

--- src/Qutell.ThreeSixty.Domain/Entities/Lookups/LocationBasedLookups/Area.cs
+++ src/Qutell.ThreeSixty.Domain/Entities/Lookups/LocationBasedLookups/Area.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Qutell.ThreeSixty.Domain.Entities.Lookups.LocationBasedLookups
 {
-    public class Area(ILogger<Area> logger) : LookupBase
+    public class Area : LookupBase
     {
         public int CityId { get; set; }
     }
